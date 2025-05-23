0f381419e558536677f930f85af9154290612687
PierreSaumet <psaumet.dvp@gmail.com>
2025-05-23 14:49:05 +0200

    feat(contact): add contact form and text

---
+++ frontend/src/components/Contact/Form.jsx
@@ -0,0 +1,71 @@
+function ContactForm() {
+  return (
+    <form class="border-main-text mx-4 w-64 max-w-3xl rounded-xl border sm:w-auto">
+      <div class="mt-5 flex flex-col items-center justify-center md:flex-row">
+        {/* LastName / Nom */}
+        <div className="mx-1 my-5 max-w-80 px-5 md:mx-10 md:my-0 md:px-0">
+          <input
+            type="text"
+            id="lastname"
+            className="border-main-text focus:border-main-text text-main-text w-full appearance-none border-b-2 p-2 text-center text-2xl focus:border focus:placeholder-transparent focus:outline-none"
+            placeholder="Nom"
+            required
+          />
+        </div>
+
+        {/* FirstName / Prenom */}
+        <div className="mx-1 my-5 max-w-80 px-5 md:mx-10 md:my-0 md:px-0">
+          <input
+            type="text"
+            id="firstname"
+            className="border-main-text focus:border-main-text text-main-text w-full appearance-none border-b-2 p-2 text-center text-2xl focus:border focus:placeholder-transparent focus:outline-none"
+            placeholder="Prénom"
+            required
+          />
+        </div>
+      </div>
+      <div class="flex flex-col items-center justify-center md:mt-5 md:flex-row">
+        {/* Other / Autre */}
+        <div className="mx-1 my-5 max-w-80 px-5 md:mx-10 md:my-0 md:px-0">
+          <input
+            type="tel"
+            id="phone"
+            className="border-main-text focus:border-main-text text-main-text w-full appearance-none border-b-2 p-2 text-center text-2xl focus:border focus:placeholder-transparent focus:outline-none"
+            placeholder="Téléphone"
+            required
+          />
+        </div>
+
+        {/* Email */}
+        <div className="mx-1 my-5 max-w-80 px-5 md:mx-10 md:my-0 md:px-0">
+          <input
+            type="email"
+            id="email"
+            className="border-main-text focus:border-main-text text-main-text w-full appearance-none border-b-2 p-2 text-center text-2xl focus:border focus:placeholder-transparent focus:outline-none"
+            placeholder="Email"
+            required
+          />
+        </div>
+      </div>
+
+      {/* Message */}
+      <div class="flex flex-col items-center justify-center md:mt-5 md:flex-row">
+        <div className="my-5 flex-auto px-5 md:mx-10 md:my-0 md:px-0">
+          <input
+            type="text"
+            id="message"
+            className="border-main-text focus:border-main-text text-main-text w-full appearance-none border-b-2 p-2 text-center text-2xl focus:border focus:placeholder-transparent focus:outline-none"
+            placeholder="Message"
+            required
+          />
+        </div>
+      </div>
+
+      <button className="bg-main-text my-10 rounded-lg px-8 py-4 text-base font-medium text-white">
+        Contact
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,23 @@
+import ContactForm from "../components/Contact/Form";
+
 function Contact() {
   return (
-    <div>
-      Contact
-    </div>
+    <section class="my-[80px] flex flex-col items-center justify-center text-center">
+      <div class="mb-10 max-w-4xl md:mb-20">
+        <h2 class="mx-5 text-4xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl">
+          Votre avis compte !
+        </h2>
+        <p class="mx-8 my-8 text-sm font-normal sm:text-base md:mx-4 md:text-lg lg:mx-0">
+          Votre retour est essentiel pour nous améliorer ! Partagez votre
+          expérience, dites-nous ce que vous aimez et ce que nous pourrions
+          améliorer. Vos suggestions nous aident à faire de ce blog une
+          ressource toujours plus utile et enrichissante.
+        </p>
+      </div>
+
+      <ContactForm />
+    </section>
   );
-};
+}
 
 export default Contact;
