5fc60101d249ebe7b8813a3162ef1cf65b00f5ec
PierreSaumet <psaumet.dvp@gmail.com>
2025-05-25 12:38:48 +0200

    feat(404): adding error page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import Layout from "./Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
+import ErrorPage from "./pages/ErrorPage";
 
 // Define children routes
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -28,7 +30,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
