7124f3cca253fc13a971bb4c2aea31e8534392a8
PierreSaumet <psaumet.dvp@gmail.com>
2026-04-28 22:12:52 +0200

    testing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,53 +2,32 @@ import ExploreWeb from "../components/Home/ExploreWeb";
 import Learn from "../components/Home/Learn";
 import StayTuned from "../components/Home/StayTuned";
 import Trust from "../components/Home/Trust";
+import { api } from "../components/UserContext";
+
 import axios from "axios";
 
 function Home() {
-  const handleSubmit = async (e) => {
+  const enfer = async (e) => {
     e.preventDefault();
-
-    try {
-      const response = await axios.post(
-        `http://127.0.0.1:8000/api/users/password-reset/request/`,
-        {
-          email: "admin@admin.com",
-        },
-      );
-
-      console.log(" ENFER ", response);
-    } catch (err) {
-      console.log("enfer ", err);
-    }
-  };
-
-  const handleSubmitProd = async (e) => {
-    e.preventDefault();
-
     try {
       const response = await axios.post(
         `https://weeb-backend-mcg6.onrender.com/api/users/password-reset/request/`,
-        {
-          email: "admin@admin.com",
-        },
+        { email: "aa.aa@gmail.com" },
+        // { headers: { "Content-Type": "application/json" } },
       );
 
-      console.log(" ENFER ", response);
-    } catch (err) {
-      console.log("enfer ", err);
+      console.log("enfer ", response);
+    } catch (error) {
+      console.error("Erreur d'inscription:", error);
     }
   };
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <button>CLIQUE</button>
-      </form>
-
-      <form onSubmit={handleSubmitProd}>
-        <button>CLIQUE PROD</button>
-      </form>
-
+      <div>
+        <form onSubmit={enfer}>
+          <button>CLIKE KEKEKEKEK</button>
+        </form>
+      </div>
       <ExploreWeb />
       <Trust />
       <Learn />
